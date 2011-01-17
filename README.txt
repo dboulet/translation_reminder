9b1cfc3738b20ec6e330c3e95f45fedaef863b27
Daniel Boulet <dan@danboulet.com>
2011-01-17 12:52:35 -0600

    Initial commit, adding files.

---
+++ README.txt
@@ -0,0 +1,16 @@
+This module adds options at the end of node forms to proceed to creating translations after saving the node. This is useful for reminding content creators to translate the content that they create or edit.
+
+INSTALLATION
+============
+- Enable the module on the Modules page
+The translation reminder will not appear at the bottom of node forms for all node types that have translation enabled.
+
+
+DEPENDENCIES
+============
+- Content translation (Core module)
+
+
+AUTHOR & MAINTAINER
+===================
+dboulet (Daniel Boulet)
---
+++ translation_reminder.js
@@ -0,0 +1,32 @@
+/* Javascript for Translation reminder module */
+
+Drupal.behaviors.translation_reminder = function() {
+  $(".node-form select#edit-language").change(function() {
+    lang = $("option:selected", this).val();
+    Drupal.translation_reminder.sync(lang);
+    $("#translation-reminder-options input").val(['']);
+  });
+  if ($("#translation-reminder-options .warning").length == 0) {
+    $('<div class="warning">'+ Drupal.t('Translation is disabled for language neutral content.') +'</div>').appendTo("#translation-reminder-options").hide();
+  }
+  var lang = $(".node-form select#edit-language option:selected").val();
+  Drupal.translation_reminder.sync(lang);
+}
+
+Drupal.translation_reminder = {
+  'sync': function(lang) {
+    if (lang != '') {
+      $("#translation-reminder-options").removeClass("disabled").find(".warning").hide().end().find("input.form-radio").each(function() {
+        if ($(this).val() == lang) {
+          $(this).attr('disabled', 'disabled').parent().hide();
+        }
+        else {
+          $(this).removeAttr('disabled').parent().show();
+        }
+      });
+    }
+    else {
+      $("#translation-reminder-options").addClass("disabled").find(".warning").show().end().find("input.form-radio").attr('disabled', 'disabled').parent().show();
+    }
+  } 
+};
