dd5382666968e54bb089b2262690d87a7ae43ac1
Dan Boulet <dan@danboulet.com>
2013-12-30 17:10:40 -0600

    Clean up and optimize JS.

--- translation_reminder.js
+++ translation_reminder.js
@@ -1,22 +1,23 @@
-/* Javascript for Translation reminder module */
+/* Javascript for Translation Reminder module */
+(function ($) {
 
-Drupal.behaviors.translation_reminder = function() {
-  $(".node-form select#edit-language").change(function() {
+Drupal.behaviors.translation_reminder = function () {
+  $(".node-form select#edit-language").change(function () {
     lang = $("option:selected", this).val();
     Drupal.translation_reminder.sync(lang);
     $("#translation-reminder-options input").val(['']);
   });
   if ($("#translation-reminder-options .warning").length == 0) {
-    $('<div class="warning">'+ Drupal.t('Translation is disabled for language neutral content.') +'</div>').appendTo("#translation-reminder-options").hide();
+    $('<div class="warning">' + Drupal.t('Translation is disabled for language neutral content.') + '</div>').appendTo("#translation-reminder-options").hide();
   }
   var lang = $(".node-form select#edit-language option:selected").val();
   Drupal.translation_reminder.sync(lang);
-}
+};
 
 Drupal.translation_reminder = {
-  'sync': function(lang) {
+  'sync': function (lang) {
     if (lang != '') {
-      $("#translation-reminder-options").removeClass("disabled").find(".warning").hide().end().find("input.form-radio").each(function() {
+      $("#translation-reminder-options").removeClass("disabled").find(".warning").hide().end().find("input.form-radio").each(function () {
         if ($(this).val() == lang) {
           $(this).attr('disabled', 'disabled').parent().hide();
         }
@@ -28,5 +29,7 @@ Drupal.translation_reminder = {
     else {
       $("#translation-reminder-options").addClass("disabled").find(".warning").show().end().find("input.form-radio").attr('disabled', 'disabled').parent().show();
     }
-  } 
+  }
 };
+
+}) (jQuery);
